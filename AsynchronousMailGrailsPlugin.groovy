496f56187347a3ee5dfc8ebffd53a05890f90951
Stefano Gualdi <stefano.gualdi@gmail.com>
2013-02-03 21:19:52 +0100

    Use SNAPSHOT release

--- AsynchronousMailGrailsPlugin.groovy
+++ AsynchronousMailGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import org.codehaus.groovy.grails.commons.spring.GrailsApplicationContext
 
 class AsynchronousMailGrailsPlugin {
 
-    def version = "1.0-RC4-LOCAL"
+    def version = "1.0-RC4-LOCAL-SNAPSHOT"
     def grailsVersion = "2.1.1 > *"
     def loadAfter = ['mail', 'hibernate']
     def loadBefore = ['quartz']
