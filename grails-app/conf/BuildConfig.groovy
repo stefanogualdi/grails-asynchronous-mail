88c8677fe6a917cdd5d1a8e124dfd7459bd7ee4c
Stefano Gualdi <stefano.gualdi@gmail.com>
2015-01-27 15:12:45 +0100

    Update plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,8 +15,8 @@ grails.project.dependency.resolution = {
             export = false
         }
 
-        compile ':mail:1.0.1'
-        compile ':quartz:1.0-RC7'
+        compile ':mail:1.0.7'
+        compile ':quartz:1.0.2'
 
         build(":release:2.2.0", ":rest-client-builder:1.0.3") {
             export = false
